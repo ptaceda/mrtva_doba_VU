2b00c7e60f677a4415d76a4bd506f56e72650c0e
Daniel_Ptacek <danielptacek7@gmail.com>
2024-07-21 20:01:35 +0200

    dodelavky ve zpracovani_vysledku

--- tew_correction.py
+++ tew_correction.py
@@ -31,11 +31,14 @@ def tew_correction(dicom_path):
     
     def perform_tew_correction(em_image, sc1_image, sc2_image):
         # Calculate scatter estimate as the average of the two scatter windows
-        scatter_estimate = (sc1_image/(413*0.06) + sc2_image/(318*0.06)) * ((364*0.2)/2)
+        # scatter_estimate = (sc1_image/(413*0.06) + sc2_image/(318*0.06)) * ((364*0.2)/2)
+        scatter_estimate = (sc1_image + sc2_image)/2
+        print(np.sum(scatter_estimate))
+        plt.imshow(scatter_estimate, cmap='gray')
         # Subtract scatter estimate from the EM image
         corrected_image = em_image - scatter_estimate
         # Ensure no negative values
-        # corrected_image = np.clip(corrected_image, 0, None)
+        corrected_image = np.clip(corrected_image, 0, None)
         return corrected_image
 
     # Perform TEW correction for each head
