62a27b13a2da6b3ec03360b7c428a5f7cd897759
Daniel Ptáček <danielptacek@Daniel-MacBook-Air-5.local>
2024-08-14 16:58:58 +0200

    uprava

--- vyber_dat_pro_fit.py
+++ vyber_dat_pro_fit.py
@@ -6,6 +6,6 @@ import os
 def vyber_dat_pro_lin_fit(aktivity, count_rate):
     aktivity_fit = [a for a in aktivity if a < 100]  # Filter elements manually
     delka = len(aktivity_fit)
-    count_rate_fit = count_rate[delka:]
+    count_rate_fit = count_rate[:-delka]
 
     return aktivity_fit, count_rate_fit
