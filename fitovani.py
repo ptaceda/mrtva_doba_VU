3810126d362656be7c53626cfd9950c2399043e8
Daniel Ptáček <danielptacek@Daniel-MacBook-Air-5.local>
2024-08-15 17:10:19 +0200

    zmena

--- fitovani.py
+++ fitovani.py
@@ -4,15 +4,13 @@ from lmfit import Model
 def linearni_fce(x, a, b):
     return a * x + b
 
-bbb = ([93.633, 62.613, 49.233, 34.167], [7.341499151033462, 5.003963281821333, 3.953806390115188, 2.840609184442201])
-
 def linearni_fit(x_a_y_data):
     aktivity = np.array(x_a_y_data[0])
     count_rate = np.array(x_a_y_data[1])
     model = Model(linearni_fce)
     params = model.make_params(a=1, b=0)
     result = model.fit(count_rate, params, x=aktivity)
-    
+
     # Extracting the best-fit values and their errors
     a = result.params['a'].value
     b = result.params['b'].value
@@ -22,4 +20,3 @@ def linearni_fit(x_a_y_data):
     # Return as a numpy array
     return np.array([a, b, a_err, b_err])
 
-print(linearni_fit(bbb))
