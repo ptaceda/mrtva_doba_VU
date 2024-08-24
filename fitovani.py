82b7fd88a46f10f6cee89c79911de06b2f9950b4
Daniel Ptáček <danielptacek@Daniel-MacBook-Air-5.local>
2024-08-24 14:21:34 +0200

    upravy

--- fitovani.py
+++ fitovani.py
@@ -23,12 +23,29 @@ def linearni_fit(x_a_y_data):
 def exp_md_fce(x, tau):
     return x * np.exp(-x * tau)
 
+def lomenna_md_fce(x, tau):
+    return x / (1 - x*tau)
+
 def exp_md_fit(x_a_y_data):
     teor_cps = np.array(x_a_y_data[0])
     mer_cps = np.array(x_a_y_data[1])
     model = Model(exp_md_fce)
     params = model.make_params(tau=0.01)
     result = model.fit(mer_cps, params, x=teor_cps)
+
+    # Extracting the best-fit values and their errors
+    tau = result.params['tau'].value
+    tau_err = result.params['tau'].stderr
+    
+    # Return as a numpy array
+    return np.array([tau, tau_err])
+
+def lomeny_md_fit(x_a_y_data):
+    mer_cps = np.array(x_a_y_data[0])
+    teor_cps = np.array(x_a_y_data[1])
+    model = Model(lomenna_md_fce)
+    params = model.make_params(tau=0.01)
+    result = model.fit(teor_cps, params, x=mer_cps)
 
     # Extracting the best-fit values and their errors
     tau = result.params['tau'].value
