07ff28921d0080d9b7491f3ce1bddf6b266822cc
Daniel_Ptacek <danielptacek7@gmail.com>
2024-09-16 15:55:18 +0200

    zmeny

--- dicom_file_separator.py
+++ dicom_file_separator.py
@@ -44,27 +44,11 @@ def separate_dicom_file_tomo(dicom_path):
     img_shape = (ds.Rows, ds.Columns)
     pixel_array = ds.pixel_array.reshape((num_images, *img_shape))
     
-    # Separate images based on their naming convention
+    # Separate images into 3 arrays
     images = {
-        'Head1_EM': None,
-        'Head1_SC1': None,
-        'Head1_SC2': None,
-        'Head2_EM': None,
-        'Head2_SC1': None,
-        'Head2_SC2': None,
-        'Acq_time' : None,
-        'Acq_date' : None
+        'PW': pixel_array[:120],
+        'SC1': pixel_array[120:240],
+        'SC2': pixel_array[240:]
     }
     
-    images['Acq_time'] = ds[0x0018, 0x1242].value*0.001
-    images['Acq_date'] = ds[0x0008, 0x0022].value
-
-    # Assuming the order in the pixel_array is known and consistent
-    images['Head1_EM'] = pixel_array[0]
-    images['Head2_EM'] = pixel_array[1]
-    images['Head1_SC1'] = pixel_array[2]
-    images['Head2_SC1'] = pixel_array[3]
-    images['Head1_SC2'] = pixel_array[4]
-    images['Head2_SC2'] = pixel_array[5]
-
     return images
