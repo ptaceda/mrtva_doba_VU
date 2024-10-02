fa6b55ac20ea4ace5835b5f9ee2229ea547ffede
Daniel Ptáček <danielptacek@Daniel-MacBook-Air-5.local>
2024-10-02 15:47:34 +0200

    zmena fitu

--- fitovani.py
+++ fitovani.py
@@ -31,7 +31,7 @@ def exp_md_fce(x, tau):
 
 def lomenna_md_fce(x, tau):
     x = np.array(list(x))
-    return x / (1 - x*tau)
+    return x / (1 + x*tau)
 
 def exp_md_fit(x_a_y_data):
     teor_cps = np.array(x_a_y_data[0])
