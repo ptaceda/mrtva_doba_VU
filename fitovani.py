f6f7a2edb1eacd026791450f17ebcec328a5e8c6
Daniel Ptáček <danielptacek@Daniel-MacBook-Air-5.local>
2024-10-02 15:58:09 +0200

    zmena fitu

--- fitovani.py
+++ fitovani.py
@@ -7,8 +7,8 @@ def linearni_fce(x, a, b):
 def kvadraticka_fce(x, a, b):
     return a * x**2 + b * x + 1
 
-def polynom_3_fce(x, a, b, c):
-    return a * x**3 + b * x**2 + c*x + 1
+def polynom_5_fce(x, a, b, c, d, e):
+    return a * x**5 + b * x**4 + c * x**3 + d*x**2 + e*x + 1
 
 def linearni_fit(x_a_y_data):
     aktivity = np.array(x_a_y_data[0])
@@ -80,26 +80,30 @@ def kvadr_fit(x_a_y_data):
     # return np.array([[a, b, c], [a_err, b_err, c_err]]) 
     return np.array([[a, b], [a_err, b_err]])
 
-def pol_3_stupne_fit(x_a_y_data):
+def pol_5_stupne_fit(x_a_y_data):
     x = np.array(x_a_y_data[0])
     y = np.array(x_a_y_data[1])
-    model = Model(polynom_3_fce)
-    params = model.make_params(a=1, b=1, c=1)
+    model = Model(polynom_5_fce)
+    params = model.make_params(a=1, b=1, c=1, d=1, e=1)
     result = model.fit(y, params, x=x)
 
     # Extracting the best-fit values and their errors
     a = result.params['a'].value
     b = result.params['b'].value
     c = result.params['c'].value
+    d = result.params['d'].value
+    e = result.params['e'].value
 
     a_err = result.params['a'].stderr
     b_err = result.params['b'].stderr
     c_err = result.params['c'].stderr
+    d_err = result.params['d'].stderr
+    e_err = result.params['e'].stderr
 
     
     # Return as a numpy array
     # return np.array([[a, b, c], [a_err, b_err, c_err]]) 
-    return np.array([[a, b, c], [a_err, b_err, c_err]])
+    return np.array([[a, b, c, d, e], [a_err, b_err, c_err, d_err, e_err]])
 
 
 
