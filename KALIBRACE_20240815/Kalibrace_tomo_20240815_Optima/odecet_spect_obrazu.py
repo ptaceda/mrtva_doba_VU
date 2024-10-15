a11f3f29c8b28bbf2c4ebb184d47aa05894bb7f1
Daniel Ptáček <danielptacek@Daniel-MacBook-Air-5.local>
2024-10-15 12:50:45 +0200

    dodelani odectu SPECT obrazu

--- KALIBRACE_20240815/Kalibrace_tomo_20240815_Optima/odecet_spect_obrazu.py
+++ KALIBRACE_20240815/Kalibrace_tomo_20240815_Optima/odecet_spect_obrazu.py
@@ -1,20 +1,33 @@
 import pydicom
 import numpy as np
+import os
+import argparse
 
 
-img_SC = "KALIBRACE_20240815/Kalibrace_tomo_20240815_Optima/Optima_AC.SC.RR_1.dcm"
-img_no_SC = "KALIBRACE_20240815/Kalibrace_tomo_20240815_Optima/Optima_AC.RR_1.dcm"
+def substract_SPECT_images(image_SC, image_no_SC):
+    ds1 = pydicom.dcmread(image_SC)
+    ds2 = pydicom.dcmread(image_no_SC)
 
-def load_dicom_files(file1, file2):
-    ds1 = pydicom.dcmread(file1)
-    ds2 = pydicom.dcmread(file2)
-
-    img1 = ds1.pixel_array
-    img2 = ds2.pixel_array
+    img_SC = ds1.pixel_array
+    img_no_SC = ds2.pixel_array
     
-    return img1, img2
+    print("Minimum v SC - no_SC: ", np.min(img_SC - img_no_SC))
+    print("Maximum v SC - no_SC: ", np.max(img_SC - img_no_SC))
+    print("Mean v SC - no_SC: ", np.mean(img_SC - img_no_SC))
+    print("STD v SC - no_SC: ", np.std(img_SC - img_no_SC))
+    return None
 
 
-image_SC, image_no_SC = load_dicom_files(img_SC, img_no_SC)
-
-print("Maximum v SC - no_SC: ", np.max(image_no_SC - image_SC))
+if __name__ == "__main__":
+    # Create the argument parser
+    parser = argparse.ArgumentParser(description="Extract photopeak window from DICOM files.")
+    
+    # Add arguments for input and output directories
+    parser.add_argument("image_SC", type=str, help="Path to the dicom image with scatter correction.")
+    parser.add_argument("Image_no_SC", type=str, help="Path to the dicom image with NO scatter correction.")
+    
+    # Parse the command-line arguments
+    args = parser.parse_args()
+    
+    # Call the function with the parsed arguments
+    substract_SPECT_images(args.image_SC, args.image_no_SC)
