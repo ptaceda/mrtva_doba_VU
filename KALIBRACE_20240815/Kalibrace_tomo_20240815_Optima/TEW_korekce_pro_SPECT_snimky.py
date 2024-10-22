d37d35a2825e1d119e599355c2373598908032e5
Daniel Ptáček <danielptacek@Daniel-MacBook-Air-5.local>
2024-10-22 15:24:13 +0200

    zmena TEW Spect

--- KALIBRACE_20240815/Kalibrace_tomo_20240815_Optima/TEW_korekce_pro_SPECT_snimky.py
+++ KALIBRACE_20240815/Kalibrace_tomo_20240815_Optima/TEW_korekce_pro_SPECT_snimky.py
@@ -19,8 +19,7 @@ def save_corrected_images_to_dicom(original_dicom_path, corrected_images, output
     num_images, rows, cols = corrected_images.shape
 
     # Update DICOM attributes to reflect 120 images
-    if hasattr(new_ds, 'NumberOfFrames'):
-        new_ds.NumberOfFrames = num_images  # Set to 120 frames (TEW-corrected)
+    new_ds.NumberOfFrames = num_images  # Set to 120 frames (TEW-corrected)
 
     new_ds.Rows, new_ds.Columns = rows, cols  # Update rows and columns based on the corrected images
 
@@ -31,30 +30,98 @@ def save_corrected_images_to_dicom(original_dicom_path, corrected_images, output
     new_ds.PixelData = corrected_images.tobytes()
 
     # Modify (0054, 0090) Angular View Vector to have 120 elements
-    if '00540090' in new_ds:
-        angular_view_vector = new_ds[0x0054, 0x0090].value
-        if len(angular_view_vector) == 360:
-            new_ds[0x0054, 0x0090].value = angular_view_vector[:120]  # Keep only the first 120 elements
+    new_ds[0x0054, 0x0090].value = new_ds[0x0054, 0x0090].value[:120]  # Keep only the first 120 elements
 
     # Modify (0054, 0020) Detector Vector to have 120 elements
-    if '00540020' in new_ds:
-        detector_vector = new_ds[0x0054, 0x0020].value
-        if len(detector_vector) == 360:
-            new_ds[0x0054, 0x0020].value = detector_vector[:120]  # Keep only the first 120 elements
+    new_ds[0x0054, 0x0020].value = new_ds[0x0054, 0x0020].value[:120]  # Keep only the first 120 elements
 
-    # Overwrite attributes that are tied to multi-energy window images
-    if hasattr(new_ds, 'EnergyWindowInformationSequence'):
-        del new_ds.EnergyWindowInformationSequence  # Remove, as TEW-corrected images don't have energy windows
+    # Modify (0054, 0050) Rotation Vector to have 120 elements
+    new_ds[(0x0054, 0x0050)].value = new_ds[(0x0054, 0x0050)].value[:120]  # Keep only the first 120 elements
 
-    if hasattr(new_ds, 'EnergyWindowRangeSequence'):
-        del new_ds.EnergyWindowRangeSequence  # Remove this as well, if present
+    if (0x0013, 0x101e) in new_ds:
+        new_ds[(0x0013, 0x101e)].value = new_ds[(0x0013, 0x101e)].value[:120]  # Keep only the first 120 elements
+    else:
+        new_ds.add_new((0x0013, 0x101e), 'FD', [0.0] * 120)  # Create a new FD tag with 120 elements of 0.0
 
-    if hasattr(new_ds, 'EnergyWindowNumber'):
-        del new_ds.EnergyWindowNumber  # Clear any reference to specific energy windows
+
+
 
     # Optionally, update other DICOM tags (e.g., SeriesDescription, StudyDate, etc.)
-    new_ds.SeriesDescription = "TEW Corrected Images"
-    new_ds.DatasetName = "TEW Corrected Images"
+    new_ds[0x0008, 0x103e].value = "TEW Corrected Images"
+
+    new_ds[0x0009, 0x1026].value = 1
+    new_ds[0x0009, 0x1045].value = '**' # predtim ve tvaru '**\\**\\**'
+
+    new_ds[0x0011, 0x1011].value = original_ds[0x0011, 0x1011].value[0]
+    new_ds[0x0011, 0x1012].value = ['TEW Corrected EM']
+    new_ds[0x0011, 0x1015].value = original_ds[0x0011, 0x1015].value[0]
+    new_ds[0x0011, 0x1016].value = original_ds[0x0011, 0x1016].value[0]
+    new_ds[0x0011, 0x1017].value = original_ds[0x0011, 0x1017].value[0]
+    new_ds[0x0011, 0x1018].value = original_ds[0x0011, 0x1018].value[0]
+    new_ds[0x0011, 0x1019].value = original_ds[0x0011, 0x1019].value[0]
+    new_ds[0x0011, 0x101a].value = original_ds[0x0011, 0x101a].value[0]
+    new_ds[0x0011, 0x101f].value = original_ds[0x0011, 0x101f].value[0]
+    new_ds[0x0011, 0x1026].value = original_ds[0x0011, 0x1026].value[0]
+    new_ds[0x0011, 0x1027].value = original_ds[0x0011, 0x1027].value[0:2]
+    new_ds[0x0011, 0x1028].value = original_ds[0x0011, 0x1028].value[0:2]
+    new_ds[0x0011, 0x102c].value = original_ds[0x0011, 0x102c].value[0]
+    new_ds[0x0011, 0x102d].value = original_ds[0x0011, 0x102d].value[0]
+    new_ds[0x0011, 0x102e].value = original_ds[0x0011, 0x102e].value[0]
+    new_ds[0x0011, 0x102f].value = original_ds[0x0011, 0x102f].value[0]
+    new_ds[0x0011, 0x1030].value = ['TEW Corrected EM']
+    new_ds[0x0011, 0x1031].value = original_ds[0x0011, 0x1031].value[0]
+    new_ds[0x0011, 0x1032].value = original_ds[0x0011, 0x1032].value[0]
+    new_ds[0x0011, 0x1033].value = original_ds[0x0011, 0x1033].value[0]
+    new_ds[0x0011, 0x1034].value = original_ds[0x0011, 0x1034].value[0]
+    new_ds[0x0011, 0x1035].value = original_ds[0x0011, 0x1035].value[0]
+    new_ds[0x0011, 0x1036].value = original_ds[0x0011, 0x1036].value[0]
+    new_ds[0x0011, 0x1037].value = '**'  # Previously in the format '**\\**\\**'
+    new_ds[0x0011, 0x1038].value = original_ds[0x0011, 0x1038].value[0]
+    new_ds[0x0011, 0x1039].value = original_ds[0x0011, 0x1039].value[0]
+    new_ds[0x0011, 0x103a].value = original_ds[0x0011, 0x103a].value[0]
+    new_ds[0x0011, 0x103b].value = original_ds[0x0011, 0x103b].value[0]
+    new_ds[0x0011, 0x103c].value = original_ds[0x0011, 0x103c].value[0]
+    new_ds[0x0011, 0x103f].value = original_ds[0x0011, 0x103f].value[0]
+    new_ds[0x0011, 0x1040].value = original_ds[0x0011, 0x1040].value[0]
+    new_ds[0x0011, 0x1041].value = original_ds[0x0011, 0x1041].value[0]
+    new_ds[0x0011, 0x1042].value = original_ds[0x0011, 0x1042].value[0]
+    new_ds[0x0011, 0x1043].value = original_ds[0x0011, 0x1043].value[0]
+    new_ds[0x0011, 0x1044].value = original_ds[0x0011, 0x1044].value[0]
+    new_ds[0x0011, 0x1045].value = original_ds[0x0011, 0x1045].value[0]
+    new_ds[0x0011, 0x1046].value = original_ds[0x0011, 0x1046].value[0]
+    new_ds[0x0011, 0x1050].value = ['TEW Corrected EM']
+    new_ds[0x0011, 0x1055].value = original_ds[0x0011, 0x1055].value[0]
+    new_ds[0x0011, 0x1056].value = original_ds[0x0011, 0x1056].value[0]
+    new_ds[0x0011, 0x1057].value = original_ds[0x0011, 0x1057].value[0:2]
+
+    new_ds[0x0013, 0x1012].value = original_ds[0x0013, 0x1012].value[0]
+    new_ds[0x0013, 0x1015].value = original_ds[0x0013, 0x1015].value[0]
+
+    new_ds[0x0033, 0x1107].value = original_ds[0x0033, 0x1107].value[0]
+
+
+    # Update Energy Window Vector (0054,0010) to have 120 elements
+    new_ds[(0x0054, 0x0010)].value = original_ds[(0x0054, 0x0010)].value[0:120]  # Update with the desired array of 120 elements
+
+    # Update the Number of Energy Windows (0054,0011) to 1
+    new_ds[(0x0054, 0x0011)].value = 1
+
+    # Update the Energy Window Information Sequence (0054,0012)
+    # Keep only the first item in the sequence and modify it
+    energy_window_info_seq = new_ds[(0x0054, 0x0012)].value
+    energy_window_info_seq[:] = [energy_window_info_seq[0]]  # Retain only the first sequence item
+
+    # Modify the retained item
+    first_window_range_seq = energy_window_info_seq[0][(0x0054, 0x0013)].value
+    first_window_range_seq[:] = [first_window_range_seq[0]]  # Retain only the first range sequence
+
+    # Update the Energy Window Lower Limit and Upper Limit
+    energy_window_info_seq[0][(0x0054, 0x0013)].value[0][(0x0054, 0x0014)].value = '327.6'
+    energy_window_info_seq[0][(0x0054, 0x0013)].value[0][(0x0054, 0x0015)].value = '400.4'
+
+    # Update the Energy Window Name
+    energy_window_info_seq[0][(0x0054, 0x0018)].value = 'I131_EM_TEW_corrected'
+
 
     # Save the new DICOM file
     new_ds.save_as(output_dicom_path)
@@ -104,7 +171,8 @@ def separate_dicom_file_tomo(dicom_path):
 num_images = 360
 
 # Load and process the DICOM file
-ds = separate_dicom_file_tomo(r"C:\Users\danie\Desktop\mrtva_doba_VU\KALIBRACE_20240815\Kalibrace_tomo_20240815_Optima\Kalibrace_tomo_2_20240815_Optima.dcm")
+path = r"/Users/danielptacek/Desktop/mrtva_doba_VU/KALIBRACE_20240815/Kalibrace_tomo_20240815_Optima/Kalibrace_tomo_2_20240815_Optima.dcm"
+ds = separate_dicom_file_tomo(path)
 #new_ds = pydicom.dcmread(r"KALIBRACE_20240815\Kalibrace_tomo_20240815_Optima\Kalibrace_zkouska_Optima_2_tomo.dcm")
 #print(new_ds)
 #plt.imshow(new_ds.pixel_array[0], cmap='gray')
@@ -128,7 +196,7 @@ else:
         corrected_images[i, :, :] = corrected_image
 
 # Save the corrected images to DICOM
-output_dicom_path = r"C:\Users\danie\Desktop\mrtva_doba_VU\KALIBRACE_20240815\Kalibrace_tomo_20240815_Optima\Kalibrace_zkouska_Optima_2_tomo.dcm"
-save_corrected_images_to_dicom(r"C:\Users\danie\Desktop\mrtva_doba_VU\KALIBRACE_20240815\Kalibrace_tomo_20240815_Optima\Kalibrace_tomo_2_20240815_Optima.dcm", corrected_images, output_dicom_path)
+output_dicom_path = r"/Users/danielptacek/Desktop/mrtva_doba_VU/KALIBRACE_20240815/Kalibrace_tomo_20240815_Optima/Kalibrace_TEW_Optima_2_tomo.dcm"
+save_corrected_images_to_dicom(path, corrected_images, output_dicom_path)
 
 
--- codes/dicom_info.py
+++ codes/dicom_info.py
@@ -2,7 +2,7 @@ import pydicom
 import matplotlib.pyplot as plt
 
 # Path to your DICOM file
-dicom_file_path = r'C:\Users\danie\Desktop\mrtva_doba_VU\KALIBRACE_20240815\Kalibrace_tomo_20240815_Optima\Kalibrace_zkouska_Optima_1_tomo.dcm'
+dicom_file_path = r'/Users/danielptacek/Desktop/mrtva_doba_VU/KALIBRACE_20240815/Kalibrace_tomo_20240815_Optima/Kalibrace_TEW_Optima_1_tomo.dcm'
 
 # Read the DICOM file
 try:
@@ -34,22 +34,22 @@ try:
     #plt.axis('off')
     #plt.show()
     
-    # Check if (0011, 1012) tag exists
-    if (0x0011, 0x1012) in dataset:
-        dataset_names = dataset[(0x0011, 0x1012)].value
+    # # Check if (0011, 1012) tag exists
+    # if (0x0011, 0x1012) in dataset:
+    #     dataset_names = dataset[(0x0011, 0x1012)].value
         
-        # Iterate over each dataset name and print dicominfo
-        for dataset_name in dataset_names:
-            print(f"Dataset Name: {dataset_name}")
+    #     # Iterate over each dataset name and print dicominfo
+    #     for dataset_name in dataset_names:
+    #         print(f"Dataset Name: {dataset_name}")
             
-            # Access elements based on your requirement
-            for elem in dataset:
-                if dataset[elem].tag.is_private:
-                    continue
-                # Print element name and value
-                print(f"{elem.name}: {dataset[elem].value}")
+    #         # Access elements based on your requirement
+    #         for elem in dataset:
+    #             if dataset[elem].tag.is_private:
+    #                 continue
+    #             # Print element name and value
+    #             print(f"{elem.name}: {dataset[elem].value}")
                 
-            print("\n")  # Separate each dataset info with a newline
+    #         print("\n")  # Separate each dataset info with a newline
             
 except Exception as e:
     print(f"Error reading DICOM file: {str(e)}")
