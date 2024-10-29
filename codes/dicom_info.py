b750367265e9f2e6d2f25c9f324b7bd8100a1d34
Daniel Ptáček <danielptacek@Daniel-MacBook-Air-5.local>
2024-10-29 11:38:07 +0100

    zmeny

--- codes/dicom_info.py
+++ codes/dicom_info.py
@@ -2,11 +2,7 @@ import pydicom
 import matplotlib.pyplot as plt
 
 # Path to your DICOM file
-<<<<<<< HEAD
-dicom_file_path = r'/Users/danielptacek/Desktop/terapky_FNKV_I-131/NOVE_VYHODNOCENI/Stroner_Josef/20241024 Stroner/ECTCT24h_EM001_DS_6degrees.dcm'
-=======
-dicom_file_path = r'C:\Users\danie\Desktop\terapky_FNKV_I-131\NOVE_VYHODNOCENI\Stroner_Josef\Stroner_Josef_pro_TK\ANT_EM007_DS.dcm'
->>>>>>> 02bb844d0b95ee595b84c26ac3d45c181973cd61
+dicom_file_path = r'/Users/danielptacek/Desktop/mrtva_doba_VU/KALIBRACE_20240815/Kalibrace_tomo_20240815_Optima/Kalibrace_tomo_3_20240815_Optima.dcm'
 
 # Read the DICOM file
 try:
@@ -14,7 +10,6 @@ try:
     print(dataset)
 
     print(dataset.pixel_array.shape)
-<<<<<<< HEAD
     # plt.subplots(2,3, figsize=(15, 10), dpi = 200)
     # plt.subplot(231)
     # plt.imshow(dataset.pixel_array[0], cmap='gray')
@@ -35,28 +30,6 @@ try:
     # plt.imshow(dataset.pixel_array[241], cmap='gray')
     # plt.axis('off')
     # plt.show()
-=======
-    #plt.subplots(2,3, figsize=(15, 10), dpi = 200)
-    #plt.subplot(231)
-    #plt.imshow(dataset.pixel_array[55], cmap='gray')
-    #plt.axis('off')
-    #plt.subplot(232)
-    #plt.imshow(dataset.pixel_array[57], cmap='gray')
-    #plt.axis('off')
-    #plt.subplot(233)
-    #plt.imshow(dataset.pixel_array[59], cmap='gray')
-    #plt.axis('off')
-    #plt.subplot(234)
-    #plt.imshow(dataset.pixel_array[61], cmap='gray')
-    #plt.axis('off')
-    #plt.subplot(235)
-    #plt.imshow(dataset.pixel_array[63], cmap='gray')
-    #plt.axis('off')
-    #plt.subplot(236)
-    #plt.imshow(dataset.pixel_array[73], cmap='gray')
-    #plt.axis('off')
-    #plt.show()
->>>>>>> 02bb844d0b95ee595b84c26ac3d45c181973cd61
     
     # # Check if (0011, 1012) tag exists
     # if (0x0011, 0x1012) in dataset:
